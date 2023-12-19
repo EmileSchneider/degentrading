164a6a58fc45a4e8ebf6b9a66d12d9ffcf63d369
Émile Schneider <emileschneider7@gmail.com>
2023-12-19 09:58:43 +0100

    initial commit

--- degentrading/settings_prod.py
+++ degentrading/settings_prod.py
@@ -14,3 +14,22 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/degentrader/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
